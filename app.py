10a2d8b268827a2f99641ee741fa54e1dfdaeb52
Saman Attar <saman.attar@outlook.com>
2018-10-02 19:51:29 -0500

    Added Sign In Functionality
    User can now sign in and get routed to a dashboard page to alert them
    of successful login

--- app.py
+++ app.py
@@ -21,9 +21,49 @@ mysql.init_app(app)
 def index():
     return render_template('index.html')
 
-@app.route('/signin')
-def signin():
+# User login
+@app.route('/signin', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('signin.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('signin.html', error=error)
+
     return render_template('signin.html')
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
