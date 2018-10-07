18c6656962a0746f9161445dde423a4c4e5ee364
Saman Attar <saman.attar@outlook.com>
2018-10-07 17:58:20 -0500

    Added Functionality to upload file
    User can now upload a file and it will be saved to the same directory
    as the program. It is not yet linked to SQL DB

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, render_template, flash, redirect, url_for, session, logging
-from flask_mysqldb import MySQL
+import os
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flask_mysqldb import MySQL
+from werkzeug.utils import secure_filename
 from passlib.hash import sha256_crypt
 #from flask.ext.mysql import MySQL
 
 
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = set(['pdf'])
+
 app = Flask('__name__')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mysql  = MySQL()
 
 #config mySQL
@@ -59,8 +65,6 @@ def login():
 
     return render_template('signin.html')
 
-
-
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
@@ -86,6 +90,28 @@ def signup():
         return render_template('signup.html', form = form)
     return render_template('signup.html', form = form)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+    return render_template('upload.html')
+
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -95,6 +121,7 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message = 'Password Does Not Match')
     ])
     confirm = PasswordField('Confrim Password')
+
 
 if __name__ == '__main__':
     app.secret_key = 'theNurseNeedsFiles18#'
